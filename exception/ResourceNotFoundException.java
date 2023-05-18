9f27b42248ab7b214e398df19d3ff98fc0a37e61
yashwant gahlot <yashwantgahlot@yashwants-MacBook-Pro.local>
2023-05-18 07:07:08 +0530

    comments for REsource Not Found Exception Added

--- exception/ResourceNotFoundException.java
+++ exception/ResourceNotFoundException.java
@@ -3,6 +3,8 @@ package com.example.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//The @ResponseStatus annotation ensures that the appropriate HTTP response status code (404)
+// is returned when this exception is thrown, indicating to the client that the requested resource was not found.
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
